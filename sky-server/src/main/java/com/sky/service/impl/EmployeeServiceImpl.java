d12e1fd2a18980200d420c6927306ca512479d36
chenggong <2273915626@qq.com>
2024-08-28 14:24:23 +0800

    分页查询，重点修改时间格式

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -46,7 +51,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+
         password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
@@ -78,7 +83,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
         //创建人id
-        //TODO 后期需要获取当前登录用户id
+        // 后期需要获取当前登录用户id
         Long userID = BaseContext.getCurrentId();
         employee.setCreateUser(userID);
         //修改人id
@@ -86,5 +91,25 @@ public class EmployeeServiceImpl implements EmployeeService {
         //保存到数据库
         employeeMapper.insert(employee);
 
+    }
+
+    /**
+     * 页面查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        //select * from employee limit 0,10;
+        //使用pagehelper
+        PageResult pageResult = new PageResult();
+        PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
+        //查询
+
+        List<Employee> list=employeeMapper.pageQuery(employeePageQueryDTO);
+        Page<Employee> p  =(Page<Employee>) list;
+        pageResult.setRecords(p);
+        pageResult.setTotal(p.getTotal());
+        return pageResult;
     }
 }
